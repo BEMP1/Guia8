58c1900ffe49a49a2cea0a3cc9efee29e1222475
Bruno Moyano <108307019+BEMP1@users.noreply.github.com>
2022-09-07 21:39:21 -0300

    Add files via upload

---
+++ ejer2guia8/src/clase/RevolverDeAgua.java
@@ -0,0 +1,37 @@
+package clase;
+
+public class RevolverDeAgua {
+
+    public int posicionActual;
+    public int posicionAgua;
+
+    public RevolverDeAgua() {
+    }
+
+    public RevolverDeAgua(int posicionActual, int posicionAgua) {
+        this.posicionActual = posicionActual;
+        this.posicionAgua = posicionAgua;
+    }
+
+    public int getPosicionActual() {
+        return posicionActual;
+    }
+
+    public void setPosicionActual(int posicionActual) {
+        this.posicionActual = posicionActual;
+    }
+
+    public int getPosicionAgua() {
+        return posicionAgua;
+    }
+
+    public void setPosicionAgua(int posicionAgua) {
+        this.posicionAgua = posicionAgua;
+    }
+
+    public void llenarRevolver() {
+        posicionActual = (int) Math.random() * 5 + 1;
+        posicionAgua = (int) Math.random() * 5 + 1;
+    }
+
+}
---
+++ ejer2guia8/src/ejer2guia8/Ejer2guia8.java
@@ -0,0 +1,9 @@
+package ejer2guia8;
+
+public class Ejer2guia8 {
+
+    public static void main(String[] args) {
+
+    }
+
+}
